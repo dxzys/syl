cdae2a74d772aede9759eabc23aa652d72723350
dxzys <76858431+dxzys@users.noreply.github.com>
2025-08-15 13:04:11 -0300

    setup express

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+const express = require('express')
 
+const app = express()
+const port = 3000
+
+app.get('/', (reeq, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log('Server running at port ${port}')
+})
